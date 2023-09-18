a72128b683249c0fcbd63cb37cdb9a4a1766b00c
Isiyok <muhammed.isiyok@siemens.com>
2023-09-18 22:21:46 +0300

    till 143: Context using createContext() and useReducer()

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,8 +1,19 @@
-import { Text } from "react-native";
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+import { ExpensesContext } from "../store/expenses-context";
+import { getDateMinusDays } from "../util/date";
+import { useContext } from "react";
 
 function RecentExpenses() {
-  return <ExpensesOutput expensesPeriod="Last 7 Days" />;
+  const expensesCtx = useContext(ExpensesContext);
+
+  const resentExpenses = expensesCtx.expenses.filter((expense) => {
+    const today = new Date();
+    const date7DaysAgo = getDateMinusDays(today, 7);
+
+    return expense.date > date7DaysAgo;
+  });
+
+  return <ExpensesOutput expenses={resentExpenses} expensesPeriod="Last 7 Days" />;
 }
 
 export default RecentExpenses;
